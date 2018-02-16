26b49ca8a5bcf2e637dccbcc3bb848c47f9a6432
dabaldassi <dabaldassi@etu.uca.fr>
2018-02-16 13:07:10 +0100

    Clean VolumeControl.java

--- RemoteControlPC-JavaFXML/src/volumecontrol/VolumeControl.java
+++ RemoteControlPC-JavaFXML/src/volumecontrol/VolumeControl.java
@@ -1,38 +1,7 @@
 package volumecontrol;
 
-import java.util.ArrayList;
-import java.lang.*;
-import javax.sound.sampled.*;
-
 public class VolumeControl
 {
-    //private ArrayList<Line> speakers = new ArrayList<Line>();
-	
-    public VolumeControl()
-    {
-	/*Mixer.Info[] mixers = AudioSystem.getMixerInfo();
-
-	for (Mixer.Info mixerInfo: mixers) {
-	    //if(!mixerInfo.getName().equals("Java Sound Audio Engine")) continue;
-	    Mixer mixer  = AudioSystem.getMixer(mixerInfo);
-	    Line.Info[] lines = mixer.getSourceLineInfo();
-
-	    for (Line.Info lineInfo : lines) {
-		try {
-		    Line line = mixer.getLine(lineInfo);
-		    speakers.add(line);
-		}
-		catch (Exception e) {
-		    System.out.println("Error " + e.getMessage());
-		    e.printStackTrace();
-		}
-
-	    }
-
-	    }*/
-
-    }
-
     public void setVolume(float level) {
 	if(level >=0 && level <= 100 && System.getProperty("os.name").equals("Linux"))
 	    {
@@ -48,14 +17,3 @@ public class VolumeControl
 
     }
 }
-
-/*
-public class VolumeControl
-{
-    public static void main(String args[])
-    {
-	Volume v = new Volume();
-	v.setVolume(35);
-    }
-}
-*/
